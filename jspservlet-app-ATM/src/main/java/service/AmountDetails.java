9a4924f9149e2cecc914891a49805d1322651d5c
root <root@beabcedfaeffedecdccfabacdfbcebcbdbeaebdaae-0.beabcedfaeffedecdccfabacdfbcebcbdbeaebdaae.che.svc.cluster.local>
2023-03-27 04:48:20 +0000

    Backup Branch before deleting

---
+++ jspservlet-app-ATM/src/main/java/service/AmountDetails.java
@@ -0,0 +1,24 @@
+package service;
+
+public class AmountDetails implements InterfaceClass {
+
+    @Override
+    public double withdrawAmount(double w1, double w2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double depositAmount(double d1, double d2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double accountBalance(double a1, double a2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+	
+}
